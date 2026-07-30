ccbfc0acdc61217ff96d64b17aaf76dbba74313f
Arash Ghorbannia <ghorbannia@icloud.com>
2026-07-30 09:18:25 -0400

    "updates"

--- build/CTestTestfile.cmake
+++ build/CTestTestfile.cmake
@@ -4,5 +4,5 @@
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
-add_test([=[anatomy_classifier]=] "/Users/aghorban/code/xcat2vtk/build/xcat2vtk_anatomy_tests")
-set_tests_properties([=[anatomy_classifier]=] PROPERTIES  _BACKTRACE_TRIPLES "/Users/aghorban/code/xcat2vtk/CMakeLists.txt;41;add_test;/Users/aghorban/code/xcat2vtk/CMakeLists.txt;0;")
+add_test("anatomy_classifier" "/Users/aghorban/code/xcat2vtk/build/xcat2vtk_anatomy_tests")
+set_tests_properties("anatomy_classifier" PROPERTIES  _BACKTRACE_TRIPLES "/Users/aghorban/code/xcat2vtk/CMakeLists.txt;41;add_test;/Users/aghorban/code/xcat2vtk/CMakeLists.txt;0;")
